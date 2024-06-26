79b8e55f68b9d1b39bb7644b587a06f038eea6ec
antoineleno <johndoe@example.com>
2024-06-26 17:56:19 +0800

    Air

--- models/review.py
+++ models/review.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models.place import Place
-from models.user import User
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
+class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    #user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """ Initializes a new user """
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        """ Returns a string representation of the instance """
-        return "[{} ({}) {}]".format(type(self).__name__, self.id, self.to_dict())
+    place_id = ""
+    user_id = ""
+    text = ""
