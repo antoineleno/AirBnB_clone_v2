9c0047da15ff17a31179ea41b79952709b6a5ac3
antoineleno <johndoe@example.com>
2024-06-26 08:08:23 +0800

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,27 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage or
-            returns the list of objects of one type of class
-        """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        if cls:
-            new_ojects = {}
-            for key, obj in FileStorage.__objects.items():
-                class_name = obj.to_dict()['__class__']
-                if (eval(class_name) is cls):
-                    new_ojects[key] = obj
-            return new_ojects
-        else:
-            return FileStorage.__objects
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -63,17 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """
-        delete obj from __objects if it is inside - if obj is
-        equal to None, the method should not do anything
-        """
-        if (obj is None):
-            return
-        key_to_delete = obj.to_dict()['__class__'] + '.' + obj.id
-        if key_to_delete in FileStorage.__objects.keys():
-            del FileStorage.__objects[key_to_delete]
