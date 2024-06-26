87cf973536a350e3233072a5416f031514269c7f
antoineleno <johndoe@example.com>
2024-06-26 17:23:01 +0800

    AirBnB

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """ User class inherits BaseModel and Base """
@@ -11,6 +12,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
     
     def __init__(self, *args, **kwargs):
         """ Initializes a new user """
