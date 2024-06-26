2460b7230b9e64d0aee8f5a397db2bf4f59f526d
antoineleno <johndoe@example.com>
2024-06-26 18:01:28 +0800

    AirBnB

--- models/review.py
+++ models/review.py
@@ -16,7 +16,3 @@ class Review(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """ Initializes a new user """
         super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        """ Returns a string representation of the instance """
-        return "[{} ({}) {}]".format(type(self).__name__, self.id, self.to_dict())
