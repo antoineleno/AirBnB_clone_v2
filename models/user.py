d5511216bcaeea840fcb11a56fa2a9a05385b8e8
antoineleno <johndoe@example.com>
2024-06-26 17:19:25 +0800

    AirBnB

--- models/user.py
+++ models/user.py
@@ -4,10 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
+    """ User class inherits BaseModel and Base """
+    __tablename__ = 'users'
+    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    
+    def __init__(self, *args, **kwargs):
+        """ Initializes a new user """
+        super().__init__(*args, **kwargs)
 
+    def __str__(self):
+        """ Returns a string representation of the instance """
+        return "[{} ({}) {}]".format(type(self).__name__, self.id, self.to_dict())
