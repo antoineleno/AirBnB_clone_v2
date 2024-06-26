9367ecfc074b4e9242e269fd17eb6283346d90b4
antoineleno <johndoe@example.com>
2024-06-26 17:54:03 +0800

    Ari

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """ User class inherits BaseModel and Base """
