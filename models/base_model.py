e8471bf2dfbce5a27985640be48f3cdba7d95dba
Amadou001 <bamadou634@gmail.com>
2024-06-26 15:22:54 +0800

    AirBnB clone - MySQL

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,8 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, DateTime
 
 
 Base = declarative_base()
@@ -13,15 +12,18 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(datetime, nullable=False,
-                                     default=datetime.utcnow())
-    updated_at = Column(datetime, nullable=False,
-                                     default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False,
+                                     default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False,
+                                     default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -57,6 +59,7 @@ class BaseModel:
         return dictionary
 
     def delete(self):
+        from models import storage
         """
         delete the current instance from the storage
         (models.storage) by calling the method delete
