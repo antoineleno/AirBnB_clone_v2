2b9b16440c4cfac7d0fca2c9d176e3444cbd4c57
antoineleno <johndoe@example.com>
2024-07-11 23:13:26 +0800

    de

---
+++ 100-clean_web_static.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+""" 3-do_deploy_web_static module"""
+
+from fabric.api import env, run, put, local
+import os
+from datetime import datetime
+
+env.hosts = ['100.25.143.96', '54.174.104.61']
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    Returns the archive path if correctly generated, otherwise None.
+    """
+    try:
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
+
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_{}.tgz".format(timestamp)
+
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        return "versions/{}".format(archive_name)
+
+    except Exception as e:
+        print("Packaging failed:", e)
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploy function: Uploads and deploys an archive to web servers
+
+    Args:
+        archive_path (string): Path of the archive file
+
+    Returns:
+        False if deployment fails
+    """
+    if not os.path.exists(archive_path):
+        print(f"Archive {archive_path} not found.")
+        return False
+
+    try:
+        my_archive = os.path.basename(archive_path)
+        put(archive_path, '/tmp/{}'.format(my_archive))
+
+        release_dir = '/data/web_static/releases/{}'.format(
+            my_archive.replace('.tgz', '')
+        )
+
+        run('mkdir -p {}'.format(release_dir))
+
+        run('tar -xzf /tmp/{} -C {}'.format(my_archive, release_dir))
+
+        run('rm /tmp/{}'.format(my_archive))
+
+        run('mv {}/web_static/* {}'.format(release_dir, release_dir))
+
+        run('rm -rf {}/web_static'.format(release_dir))
+
+        current_link = '/data/web_static/current'
+        run('rm -rf {}'.format(current_link))
+
+        run('ln -s {} {}'.format(release_dir, current_link))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print("Deployment failed:", e)
+        return False
+
+
+def deploy():
+    """Full deployment"""
+    archive_path = do_pack()
+    if archive_path:
+        return do_deploy(archive_path)
+    else:
+        return False
+
+
+def do_clean(number=0):
+    """
+    Cleans up old deployments and archives
+    """
+    try:
+        number = int(number)
+        if number < 1:
+            number = 1
+
+        # Local clean up of archives
+        versions_dir = 'versions'
+        local('ls -t {}/ | tail -n +{} | xargs -I {{}} rm -f {}/{{}}'.format(versions_dir, number + 1, versions_dir))
+
+        # Remote clean up of releases
+        releases_dir = '/data/web_static/releases'
+        run('ls -t {} | tail -n +{} | xargs -I {{}} rm -rf {}/{{}}'.format(releases_dir, number + 1, releases_dir))
+
+    except ValueError:
+        pass
