52d8d5ab1bf9bba8168c5abc3fc37010c5111212
antoineleno <johndoe@example.com>
2024-07-11 17:30:24 +0800

    web static deployment

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" 2-do_deploy_web_static module"""
+
+from fabric.api import env, run, put
+import os
+
+
+env.hosts = ['528902-web-01', '528902-web-02']
+
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+    
+    try:
+        my_archive = os.path.basename(archive_path)
+        put(archive_path, '/tmp/{}'.format(my_archive))
+
+        release_dir = '/data/web_static/releases/{}'.format(
+            my_archive.replace('.tgz', '')
+        )
+
+        run('mkdir -p {}'.format(release_dir))
+
+        run('tar -xzf /tmp/{} -C {}'.format(my_archive, release_dir))
+
+        run('rm /tmp/{}'.format(my_archive))
+
+        run('mv {}/web_static/* {}'.format(release_dir, release_dir))
+
+        run('rm -rf {}/web_static'.format(release_dir))
+
+        current_link = '/data/web_static/current'
+        run('rm -rf {}'.format(current_link))
+
+        run('ln -s {} {}'.format(release_dir, current_link))
+
+        print("New version deployed!")
+        return True
+    
+    except Exception as e:
+        print("Deployment failed:", e)
+        return False
